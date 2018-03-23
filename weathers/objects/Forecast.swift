7c7adac97202c055014849a6b2e0b07d277e6c05
yolandameow <32379440+yolandameow@users.noreply.github.com>
2018-03-23 00:09:52 -0400

    modified folder name

---
+++ weathers/objects/Forecast.swift
@@ -0,0 +1,33 @@
+//
+//  Forecast.swift
+//  weathers
+//
+//  Created by Heqi on 2018-03-22.
+//  Copyright © 2018 utoronto. All rights reserved.
+//
+
+import Foundation
+
+import ObjectMapper
+class Forecast:Mappable{
+    
+    var id:String?
+    var weather_state_name:String?
+    var weather_state_abbr:String?
+    var applicable_date:String?
+    var the_temp:String?
+    required init?(map: Map){
+        
+    }
+
+    func mapping(map: Map) {
+        id <- map["id"]
+        weather_state_name <- map["weather_state_name"]
+        weather_state_abbr <- map["weather_state_abbr"]
+        applicable_date <- map["applicable_date"]
+        the_temp <- map["the_temp"]
+        
+        
+    }
+    
+}
